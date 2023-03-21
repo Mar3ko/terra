c4237a9d35030416185db81534a8d3702dd96396
Mark Song <marksong172@gmail.com>
2023-03-20 20:18:52 -0600

    fix

--- provider.tf
+++ provider.tf
@@ -10,6 +10,4 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA5H62JAYT4VYFS3US"
-  secret_key = "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"
 }
